5b0d99e46e5df2a635a5ac4dd3e90bb43bdeb130
AllexKali <naidynow.alexander@yandex.ru>
2021-04-22 00:27:04 +0300

    init DB, connect to DB and server listening

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
+const dbService = require('./dbService')
 
 dotenv.config();
 
-
 app.get('/', (req, res) => res.send('Hello World!'))
+
+dbService.getSelect()
 
 app.listen(process.env.PORT, () => console.log(`Server has been started on port ${process.env.PORT}`))
---
+++ dbService.js
@@ -0,0 +1,57 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+let instance = null;
+
+
+const conn = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+})
+
+conn.connect(err => {
+    if (err) {
+        console.log(err);
+        return err;
+    } else {
+        console.log(' DB is OK');
+        console.log('db: ', conn.state);
+    }
+})
+
+class DbService {
+    //     static getDbServiceInstanse(){
+    // return instance ? instance : new DbService();
+    //     }
+    static getSelect(){
+    let query = "SELECT * FROM `role`;";
+
+    conn.query(query, (err, result, field) => {
+        console.log(err);
+        console.log(result);
+        // console.log(field);
+    })
+}
+}
+
+module.exports = DbService;
+
+
+
+
+// conn.end(err=>{
+//     if (err){
+//         console.log(err);
+//         return err;
+// }
+//     else{
+//         console.log(' DB is OK');
+//     }
+// })
+
+// module.exports = DbService;
